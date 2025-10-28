05f0de6b31af0a4435fe3c14723eb62ee3ef04ba
zouhourab1996-stack <zouhourab1996@gmail.com>
2025-10-29 00:24:39 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // ← مهم جداً لـ GitHub Pages
   server: {
     host: "::",
     port: 8080,
