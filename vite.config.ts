57ed23e38029db45e2950edc6de5855655c88e1c
zouhourab1996-stack <zouhourab1996@gmail.com>
2025-10-31 23:40:04 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/",
   server: {
@@ -44,8 +43,5 @@ export default defineConfig(({ mode }) => ({
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom'],
-  },
-  define: {
-    'process.env': {} // لتجنب مشاكل بعض المكتبات عند البناء على GitHub Pages
   },
 }));
