efe597cebe3d424e103c32b26e2f17bbef00d73c
zouhourab1996-stack <zouhourab1996@gmail.com>
2025-10-31 23:24:40 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/free-ads-tools-hub-classifed/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
