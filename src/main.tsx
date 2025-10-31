c7af0d613106df42fdd80937fd2c8fc986d4a040
zouhourab1996-stack <zouhourab1996@gmail.com>
2025-10-31 23:27:37 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </HelmetProvider>
   </StrictMode>
 );
